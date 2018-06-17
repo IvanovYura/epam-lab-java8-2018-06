c0a721457e54e5dc7f68f0c45c3eab474592c60a
Iurii Ivanov <yuran2016@gmail.com>
2018-06-17 15:59:45 +0300

    corrected 1 test

--- src/test/java/lambda/part1/exercise/Exercise1.java
+++ src/test/java/lambda/part1/exercise/Exercise1.java
@@ -18,12 +18,14 @@ public class Exercise1 {
     public void sortPersonsByAgeUsingArraysSortComparator() {
         Person[] persons = getPersons();
 
-        Arrays.sort(persons, new Comparator<Person>() {
+        Comparator<Person> comparator = new Comparator<Person>() {
             @Override
             public int compare(Person o1, Person o2) {
                 return Integer.compare(o1.getAge(), o2.getAge());
             }
-        });
+        };
+
+        Arrays.sort(persons, comparator);
 
         assertArrayEquals(new Person[]{
                 new Person("Иван", "Мельников", 20),
